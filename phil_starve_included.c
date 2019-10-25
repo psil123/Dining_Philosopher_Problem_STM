09142e409ace9fbd77d8dbb53edbeaec65c441df
arjya11 <42600095+arjya11@users.noreply.github.com>
2019-10-25 22:02:45 +0530

    Update phil_starve_included.c

--- phil_starve_included.c
+++ phil_starve_included.c
@@ -1,212 +1,248 @@
-#include <stdio.h>
-#include <pthread.h>
-#include "phil_starve_included.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
 #define THINKING 0
 #define HUNGRY 1
 #define EATING 2
 
-typedef struct {
-  pthread_mutex_t *mon;
-  pthread_cond_t **cv;
-  int *state;
-  int phil_count;
-} Phil;
-
-
-void pickup(Phil_struct *ps)
+typedef struct 
 {
-  Phil *pp;
-  int phil_count;
+	int id;
+	long program_starting_time,maximum_sleeping_time;
+	int *total_blocking_time,*block_starting_time;
+	void *v;
+	int philosopher_count;
+	pthread_mutex_t *blocktime_monitor;
+} philosopher_structure;
 
-  pp = (Phil *) ps->v;
-  phil_count = pp->phil_count;
+typedef struct
+{
+	int *state;
+	int philosopher_count;
+	pthread_mutex_t *monitor;
+	pthread_cond_t **checker;	
+} philosopher;
+
+
+void take_fork(philosopher_structure *ps)
+{
+  	philosopher *pp;
+  	int philosopher_count;
+
+	pp=(philosopher*)ps->v;
+  	philosopher_count=pp->philosopher_count;
   
-  pthread_mutex_lock(pp->mon);
-  pp->state[ps->id] = HUNGRY;
-  while (pp->state[(ps->id + (phil_count-1))%phil_count] == EATING ||
-         pp->state[(ps->id + 1)%phil_count] == EATING) {
-    pthread_cond_wait(pp->cv[ps->id], pp->mon);
-  }
-  pp->state[ps->id] = EATING;
-  pthread_mutex_unlock(pp->mon);
+  	pthread_mutex_lock(pp->monitor);
+ 	pp->state[ps->id]=HUNGRY;
+  	while (pp->state[(ps->id+(philosopher_count-1))%philosopher_count]==EATING || pp->state[(ps->id + 1)%philosopher_count]==EATING) 
+	{
+    		pthread_cond_wait(pp->checker[ps->id],pp->monitor);
+  	}
+  	pp->state[ps->id]=EATING;
+  	pthread_mutex_unlock(pp->monitor);
 }
 
-void putdown(Phil_struct *ps)
+void put_fork(philosopher_structure *ps)
 {
-  Phil *pp;
-  int phil_count;
+  	philosopher *pp;
+  	int philosopher_count;
 
-  pp = (Phil *) ps->v;
-  phil_count = pp->phil_count;
+ 	pp=(philosopher*)ps->v;
+  	philosopher_count=pp->philosopher_count;
 
-  pthread_mutex_lock(pp->mon);
-  pp->state[ps->id] = THINKING;
-  if (pp->state[(ps->id+(phil_count-1))%phil_count] == HUNGRY) {
-    pthread_cond_signal(pp->cv[(ps->id+(phil_count-1))%phil_count]);
-  }
-  if (pp->state[(ps->id+1)%phil_count] == HUNGRY) {
-    pthread_cond_signal(pp->cv[(ps->id+1)%phil_count]);
-  }
-  pthread_mutex_unlock(pp->mon);
+  	pthread_mutex_lock(pp->monitor);
+  	pp->state[ps->id]=THINKING;
+  	if(pp->state[(ps->id+(philosopher_count-1))%philosopher_count]==HUNGRY) 
+	{
+    		pthread_cond_signal(pp->checker[(ps->id+(philosopher_count-1))%philosopher_count]);
+  	}
+  	if(pp->state[(ps->id+1)%philosopher_count]==HUNGRY) 
+	{
+    		pthread_cond_signal(pp->checker[(ps->id+1)%philosopher_count]);
+  	}
+  	pthread_mutex_unlock(pp->monitor);
 }
 
-void *initialize_v(int phil_count)
+void *initialize_v(int philosopher_count)
 {
-  Phil *pp;
-  int i;
+  	philosopher *pp;
+  	int i;
 
-  pp = (Phil *) malloc(sizeof(Phil));
-  pp->phil_count = phil_count;
-  pp->mon = (pthread_mutex_t *) malloc(sizeof(pthread_mutex_t));
-  pp->cv = (pthread_cond_t **) malloc(sizeof(pthread_cond_t *)*phil_count);
-  if (pp->cv == NULL) { perror("malloc"); exit(1); }
-  pp->state = (int *) malloc(sizeof(int)*phil_count);
-  if (pp->state == NULL) { perror("malloc"); exit(1); }
-  pthread_mutex_init(pp->mon, NULL);
-  for (i = 0; i < phil_count; i++) {
-    pp->cv[i] = (pthread_cond_t *) malloc(sizeof(pthread_cond_t));
-    if (pp->cv[i] == NULL) { perror("malloc"); exit(1); }
-    pthread_cond_init(pp->cv[i], NULL);
-    pp->state[i] = THINKING;
-  }
-
-  return (void *) pp;
+  	pp = (philosopher*) malloc(sizeof(philosopher));
+  	pp->philosopher_count=philosopher_count;
+  	pp->monitor=(pthread_mutex_t*)malloc(sizeof(pthread_mutex_t));
+  	pp->checker=(pthread_cond_t**)malloc(philosopher_count*sizeof(pthread_cond_t *));
+  	if(pp->checker==NULL) 
+	{ 
+		perror("malloc"); 
+		exit(1); 
+	}
+  	pp->state=(int*)malloc(philosopher_count*sizeof(int));
+  	if(pp->state==NULL) 
+	{ 
+		perror("malloc");
+		exit(1); 
+	}
+  	pthread_mutex_init(pp->monitor, NULL);
+  	for(i=0;i<philosopher_count;i++) 
+	{
+    		pp->checker[i]=(pthread_cond_t*)malloc(sizeof(pthread_cond_t));
+    		if(pp->checker[i]==NULL) 
+		{ 
+			perror("malloc"); 
+			exit(1); 
+		}
+    		pthread_cond_init(pp->checker[i], NULL);
+    		pp->state[i] = THINKING;
+  	}
+	return (void*)pp;
 }
 
 void *philosopher(void *v)
 {
-  Phil_struct *ps;
-  long st;
-  long t;
+  	philosopher_structure *ps;
+  	long st,t;
 
-  ps = (Phil_struct *) v;
+	ps=(philosopher_structure*)v;
 
-  while(1) {
+	while(1) 
+	{
+		st = (random()%ps->maximum_sleeping_time) + 1;
+    		printf("%3d Philosopher %d thinking for %d second%s\n", time(0)-ps->program_starting_time, ps->id, st, (st == 1) ? "" : "s");
+    		fflush(stdout);
+    		sleep(st);
 
-    /* First the philosopher thinks for a random number of seconds */
+    		printf("%3d Philosopher %d no longer thinking -- calling pickup()\n",time(0)-ps->program_starting_time, ps->id);
+    		fflush(stdout);
+    		t = time(0);
+    		pthread_mutex_lock(ps->blocktime_monitor);
+    		ps->block_starting_time[ps->id] = t;
+    		pthread_mutex_unlock(ps->blocktime_monitor);
 
-    st = (random()%ps->ms) + 1;
-    printf("%3d Philosopher %d thinking for %d second%s\n", 
-                time(0)-ps->t0, ps->id, st, (st == 1) ? "" : "s");
-    fflush(stdout);
-    sleep(st);
+    		take_fork(ps);
 
-    /* Now, the philosopher wakes up and wants to eat.  He calls pickup
-       to pick up the chopsticks */
+    		pthread_mutex_lock(ps->blocktime_monitor);
+    		ps->total_blocking_time[ps->id] += (time(0) - t);
+    		ps->block_starting_time[ps->id] = -1;
+    		pthread_mutex_unlock(ps->blocktime_monitor);
 
-    printf("%3d Philosopher %d no longer thinking -- calling pickup()\n", 
-            time(0)-ps->t0, ps->id);
-    fflush(stdout);
-    t = time(0);
-    pthread_mutex_lock(ps->blockmon);
-    ps->blockstarting[ps->id] = t;
-    pthread_mutex_unlock(ps->blockmon);
+    
+    		st = (random()%ps->maximum_sleeping_time) + 1;
+    		printf("%3d Philosopher %d eating for %d second%s\n",time(0)-ps->program_starting_time, ps->id, st, (st == 1) ? "" : "s");
+    		fflush(stdout);
+    		sleep(st);
 
-    pickup(ps);
-
-    pthread_mutex_lock(ps->blockmon);
-    ps->blocktime[ps->id] += (time(0) - t);
-    ps->blockstarting[ps->id] = -1;
-    pthread_mutex_unlock(ps->blockmon);
-
-    /* When pickup returns, the philosopher can eat for a random number of
-       seconds */
-
-    st = (random()%ps->ms) + 1;
-    printf("%3d Philosopher %d eating for %d second%s\n", 
-                time(0)-ps->t0, ps->id, st, (st == 1) ? "" : "s");
-    fflush(stdout);
-    sleep(st);
-
-    /* Finally, the philosopher is done eating, and calls putdown to
-       put down the chopsticks */
-
-    printf("%3d Philosopher %d no longer eating -- calling putdown()\n", 
-            time(0)-ps->t0, ps->id);
-    fflush(stdout);
-    putdown(ps);
-  }
+    		printf("%3d Philosopher %d no longer eating -- calling putdown()\n",time(0)-ps->program_starting_time, ps->id);
+    		fflush(stdout);
+   		put_fork(ps);
+  	}
 }
 
-main(argc, argv)
-int argc; 
-char **argv;
+int main(int argc,char **argv)
 {
-  int i;
-  pthread_t *threads;
-  Phil_struct *ps;
-  void *v;
-  long t0, ttmp, ttmp2;
-  pthread_mutex_t *blockmon;
-  int *blocktime;
-  int *blockstarting;
-  char s[500];
-  int phil_count;
-  char *curr;
-  int total;
+	pthread_t *threads;
+	philosopher_structure *ps;
+	int i;
+	void *v;
+	long program_starting_time,t_temp1,t_temp2;
+	int *total_blocking_time,*block_starting_time;
+	int philosopher_count;
+	pthread_mutex_t *blocktime_monitor;
+	char s[500];
+	int total;
+	char *current;
 
-  if (argc != 3) {
-    fprintf(stderr, "usage: dphil philosopher_count maxsleepsec\n");
-    exit(1);
-  }
+	if(argc != 3) 
+	{
+    		fprintf(stderr, "Invalid Arugument!\n");
+    		exit(1);
+  	}
 
-  srandom(time(0));
-  
-  phil_count = atoi(argv[1]);
-  threads = (pthread_t *) malloc(sizeof(pthread_t)*phil_count);
-  if (threads == NULL) { perror("malloc"); exit(1); }
-  ps = (Phil_struct *) malloc(sizeof(Phil_struct)*phil_count);
-  if (ps == NULL) { perror("malloc"); exit(1); }
-  
-   
-  v = initialize_v(phil_count);
-  t0 = time(0);
-  blocktime = (int *) malloc(sizeof(int)*phil_count);
-  if (blocktime == NULL) { perror("malloc blocktime"); exit(1); }
-  blockstarting = (int *) malloc(sizeof(int)*phil_count);
-  if (blockstarting == NULL) { perror("malloc blockstarting"); exit(1); }
+	srandom(time(0));
 
-  blockmon = (pthread_mutex_t *) malloc(sizeof(pthread_mutex_t));
-  pthread_mutex_init(blockmon, NULL);
-  for (i = 0; i < phil_count; i++) {
-    blocktime[i] = 0;
-    blockstarting[i] = -1;
-  }
+	
+  	philosopher_count=atoi(argv[1]);
+  	threads=(pthread_t*)malloc(philosopher_count*sizeof(pthread_t));
+  	if(threads==NULL) 
+	{ 
+		perror("malloc"); 
+		exit(1); 
+	}
+  	ps=(philosopher_structure*)malloc(philosopher_count*sizeof(philosopher_structure));
+  	if(ps==NULL) 
+	{ 
+		perror("malloc"); 
+		exit(1); 
+	}
+	
+	v = initialize_v(philosopher_count);
+  	program_starting_time=time(0);
+  	total_blocking_time=(int*)malloc(philosopher_count*sizeof(int));
+  	if(total_blocking_time==NULL) 
+	{ 
+		perror("malloc total_blocking_time"); 
+		exit(1); 
+	}
+  	block_starting_time=(int*)malloc(philosopher_count*sizeof(int));
+  	if(block_starting_time==NULL) 
+	{ 
+		perror("malloc block_starting_time"); 
+		exit(1); 
+	}
 
-  for (i = 0; i < phil_count; i++) {
-    ps[i].id = i;
-    ps[i].t0 = t0;
-    ps[i].v = v;
-    ps[i].ms = atoi(argv[2]);
-    ps[i].blocktime = blocktime;
-    ps[i].blockstarting = blockstarting;
-    ps[i].blockmon = blockmon;
-    ps[i].phil_count = phil_count;
-    pthread_create(threads+i, NULL, philosopher, (void *) (ps+i));
-  }
+	blocktime_monitor=(pthread_mutex_t*)malloc(sizeof(pthread_mutex_t));
+  	pthread_mutex_init(blocktime_monitor,NULL);
+        for(i=0;i<philosopher_count;i++) 
+	{
+		total_blocking_time[i]=0;
+    		block_starting_time[i]=-1;
+  	}
+	
+	for (i=0;i<philosopher_count;i++) 
+	{
+    		ps[i].id = i;
+    		ps[i].program_starting_time=program_starting_time;
+    		ps[i].v = v;
+    		ps[i].maximum_sleeping_time = atoi(argv[2]);
+    		ps[i].total_blocking_time=total_blocking_time;
+    		ps[i].block_starting_time=block_starting_time;
+    		ps[i].blocktime_monitor=blocktime_monitor;
+    		ps[i].philosopher_count=philosopher_count;
+    		pthread_create(threads+i,NULL,philosopher,(void *)(ps+i));
+  	}
 
-  while(1) {
-    pthread_mutex_lock(blockmon);
-    ttmp = time(0);
-    curr = s;
-    total = 0;
-    for(i=0; i < phil_count; i++) {
-      total += blocktime[i];
-      if (blockstarting[i] != -1) total += (ttmp - blockstarting[i]);
-    }
-    sprintf(curr,"%3d Total blocktime: %5d : ", ttmp-t0, total);
+	while(1) 
+	{
+    		pthread_mutex_lock(blocktime_monitor);
+    		t_temp1 = time(0);
+    		current = s;
+    		total = 0;
+    		for(i=0;i<philosopher_count;i++) 
+		{
+      			total += total_blocking_time[i];
+      			if(block_starting_time[i]!=-1) 
+			{	
+				total += (t_temp1-block_starting_time[i]);
+    			}
+		}
+    		sprintf(current,"%3d Total blocktime: %5d : ", t_temp1-program_starting_time,total);
 
-    curr = s + strlen(s);
+    		current = s + strlen(s);
 
-    for(i=0; i < phil_count; i++) {
-      ttmp2 = blocktime[i];
-      if (blockstarting[i] != -1) ttmp2 += (ttmp - blockstarting[i]);
-      sprintf(curr, "%5d ", ttmp2);
-      curr = s + strlen(s);
-    }
-    pthread_mutex_unlock(blockmon);
-    printf("%s\n", s);
-    fflush(stdout);
-    sleep(10);
-  }
+   		for(i=0;i<philosopher_count;i++) 
+		{
+      			t_temp2 = total_blocking_time[i];
+      			if(block_starting_time[i]!=-1) 
+			{
+				t_temp2 += (t_temp2 - block_starting_time[i]);
+			}
+      			sprintf(current, "%5d ", t_temp2);
+      			current=s+strlen(s);
+    		}
+    		pthread_mutex_unlock(blocktime_monitor);
+    		printf("%s\n", s);
+    		fflush(stdout);
+    		sleep(10);
+ 	 }
 }
